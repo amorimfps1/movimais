299bce800d7d60c66f8ac9988f1fa8a4e63a8717
amorimfps1 <dantasjgabriel@gmail.com>
2026-03-23 15:35:18 +0000

    Movi+ initial rev UI and store

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import AppLayout from "@/components/AppLayout";
+import Dashboard from "@/pages/Dashboard";
+import AlunosPage from "@/pages/AlunosPage";
+import LeadsPage from "@/pages/LeadsPage";
+import MatriculasPage from "@/pages/MatriculasPage";
+import TurmasPage from "@/pages/TurmasPage";
+import ModalidadesPage from "@/pages/ModalidadesPage";
+import InstrutoresPage from "@/pages/InstrutoresPage";
+import PagamentosPage from "@/pages/PagamentosPage";
+import PresencasPage from "@/pages/PresencasPage";
+import AulasPage from "@/pages/AulasPage";
+import NotFound from "@/pages/NotFound";
+import { seedDefaults } from "@/lib/store";
+
+seedDefaults();
 
 const queryClient = new QueryClient();
 
@@ -15,8 +28,18 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/alunos" element={<AlunosPage />} />
+            <Route path="/leads" element={<LeadsPage />} />
+            <Route path="/matriculas" element={<MatriculasPage />} />
+            <Route path="/turmas" element={<TurmasPage />} />
+            <Route path="/modalidades" element={<ModalidadesPage />} />
+            <Route path="/instrutores" element={<InstrutoresPage />} />
+            <Route path="/pagamentos" element={<PagamentosPage />} />
+            <Route path="/presencas" element={<PresencasPage />} />
+            <Route path="/aulas" element={<AulasPage />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
